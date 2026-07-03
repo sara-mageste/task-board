664cd95f7c8e1b39141f36238ca9710662608aa8
Sara Mageste <sara.s.mageste@gmail.com>
2026-07-03 07:30:03 -0300

    chore: initialize project structure and backend

---
+++ backend/src/main/java/com/saramageste/taskboard/TaskBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.saramageste.taskboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBoardApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/saramageste/taskboard/TaskBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saramageste.taskboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
