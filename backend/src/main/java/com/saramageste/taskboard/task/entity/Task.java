2c594f335cfe4dc8a092f6b82e094d39598e87ac
Sara Mageste <sara.s.mageste@gmail.com>
2026-07-03 08:46:19 -0300

    feat: create task entity with enums and audit fields

---
+++ backend/src/main/java/com/saramageste/taskboard/task/entity/Task.java
@@ -0,0 +1,59 @@
+package com.saramageste.taskboard.task.entity;
+
+import com.saramageste.taskboard.task.enums.Priority;
+import com.saramageste.taskboard.task.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(nullable = false)
+    private String assignee;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Priority priority;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+
+        if (this.status == null) {
+            this.status = Status.BACKLOG;
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
