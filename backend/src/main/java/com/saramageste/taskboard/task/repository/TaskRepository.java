b908f10d9943c4436ad647ddc22ae03b474cc353
Sara Mageste <sara.s.mageste@gmail.com>
2026-07-03 08:52:12 -0300

    feat: add task repository with query methods

---
+++ backend/src/main/java/com/saramageste/taskboard/task/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.saramageste.taskboard.task.repository;
+
+import com.saramageste.taskboard.task.entity.Task;
+import com.saramageste.taskboard.task.enums.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(Status status);
+    List<Task> findByTitleContainingIgnoreCase(String title);
+}
